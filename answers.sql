4b6c158de2ee2bc56f152e20aee85495ea45e271
Abdulmumin Yusuf <abdulmuminyusuf10@gmail.com>
2025-09-14 10:14:15 +0100

    Add files via upload
    week 4 mysql assignemt

---
+++ answers.sql
@@ -0,0 +1,31 @@
+1. 
+use salesdb;
+select * from payments;
+select paymentDate, amount from payments
+order by
+ paymentDate desc;
+
+ 2. 
+ select customerName, country,
+avg(creditLimit) as average_credit_limit
+from customers
+GROUP BY 
+    customerName, 
+    country
+    
+3. 
+use salesdb;
+select productCode, quantityOrdered,
+sum(priceEach) as total_price
+from orderdetails
+GROUP BY 
+    productCode, 
+    quantityOrdered
+
+4. 
+use salesdb;
+select checkNumber,
+max(checkNumber) as highest_amount
+from payments
+GROUP BY 
+    checkNumber
